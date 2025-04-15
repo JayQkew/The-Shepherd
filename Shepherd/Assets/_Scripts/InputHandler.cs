0bcf03cb1c43434faeaff7bd0b77a288a2bcc6c8
JayQkew <rayjebb@gmail.com>
2025-04-16 00:27:21 +0200

    aim position

--- Shepherd/Assets/_Scripts/InputHandler.cs
+++ Shepherd/Assets/_Scripts/InputHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -5,35 +6,47 @@ using UnityEngine.InputSystem;
 public class InputHandler : MonoBehaviour
 {
     public Vector2 move;
-    public Vector2 aim;
-    public float zoom;
+    public Vector3 aim;
+    public Vector2 zoom;
     [SerializeField] private bool isJumping;
     [SerializeField] private bool isCrouching;
     [SerializeField] private bool isSprinting;
     [SerializeField] private bool isBarking;
     [SerializeField] private bool isInteracting;
-    
+    [SerializeField] private Camera cam;
+
     [Header("Events")]
     public UnityEvent OnJump;
+
     [Space(25)]
     public UnityEvent OnCrouch;
+
     [Space(25)]
     public UnityEvent OnSprint;
+
     [Space(25)]
     public UnityEvent OnBark;
+
     [Space(25)]
     public UnityEvent OnInteract;
-    
+
     public void Move(InputAction.CallbackContext ctx) => move = ctx.ReadValue<Vector2>();
 
-    public void Aim(InputAction.CallbackContext ctx) => aim = Camera.main.ScreenToWorldPoint(ctx.ReadValue<Vector2>());
+    public void Aim(InputAction.CallbackContext ctx) {
+        Vector2 mousePos = ctx.ReadValue<Vector2>();
+        Ray ray = cam.ScreenPointToRay(mousePos);
+        Physics.Raycast(ray, out RaycastHit hit);
+        aim = hit.point;
+    }
 
-    public void Zoom(InputAction.CallbackContext ctx) => zoom = ctx.ReadValue<float>();
+    public void Zoom(InputAction.CallbackContext ctx) => zoom = ctx.ReadValue<Vector2>();
+
     public void Jump(InputAction.CallbackContext ctx) {
         if (ctx.performed) {
             OnJump?.Invoke();
             isJumping = true;
-        } else if (ctx.canceled) {
+        }
+        else if (ctx.canceled) {
             isJumping = false;
         }
     }
@@ -42,7 +55,8 @@ public class InputHandler : MonoBehaviour
         if (ctx.performed) {
             OnCrouch?.Invoke();
             isCrouching = true;
-        } else if (ctx.canceled) {
+        }
+        else if (ctx.canceled) {
             isCrouching = false;
         }
     }
@@ -51,7 +65,8 @@ public class InputHandler : MonoBehaviour
         if (ctx.performed) {
             OnSprint?.Invoke();
             isSprinting = true;
-        } else if (ctx.canceled) {
+        }
+        else if (ctx.canceled) {
             isSprinting = false;
         }
     }
@@ -60,7 +75,8 @@ public class InputHandler : MonoBehaviour
         if (ctx.performed) {
             OnBark?.Invoke();
             isBarking = true;
-        } else if (ctx.canceled) {
+        }
+        else if (ctx.canceled) {
             isBarking = false;
         }
     }
@@ -69,9 +85,9 @@ public class InputHandler : MonoBehaviour
         if (ctx.performed) {
             OnInteract?.Invoke();
             isInteracting = true;
-        } else if (ctx.canceled) {
+        }
+        else if (ctx.canceled) {
             isInteracting = false;
         }
     }
-
 }
