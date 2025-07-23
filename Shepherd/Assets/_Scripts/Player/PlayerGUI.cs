28a2a5691fe03c86e817f5c60780f69a7e98b27b
JayQkew <rayjebb@gmail.com>
2025-07-24 01:15:29 +0200

    added trotting animation

--- Shepherd/Assets/_Scripts/Player/PlayerGUI.cs
+++ Shepherd/Assets/_Scripts/Player/PlayerGUI.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 public class PlayerGUI : MonoBehaviour
 {
     private InputHandler inputHandler;
-    private Rigidbody rb;
+    private Animator anim;
 
     private void Start() {
-        rb = GetComponentInParent<Rigidbody>();
         inputHandler = GetComponentInParent<InputHandler>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update() {
         if (inputHandler.move.x > 0) transform.localScale = new Vector3(1, 1, 1);
         else if (inputHandler.move.x < 0) transform.localScale = new Vector3(-1, 1, 1);
+        
+        if (inputHandler.move != Vector3.zero && inputHandler.isSprinting) anim.SetInteger("Move", 2);
+        else if (inputHandler.move != Vector3.zero) anim.SetInteger("Move", 1);
+        else anim.SetInteger("Move", 0);
     }
 }
