d14f83644eaa9be7676c8a37e56dc431d7978c48
JayQkew <rayjebb@gmail.com>
2025-07-26 17:22:41 +0200

    sheep logic

---
+++ Shepherd/Assets/_Scripts/Creatures/Sheep/SheepStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SheepStats", menuName = "Creatures/SheepStats")]
+public class SheepStats : ScriptableObject
+{
+    public Vector2 idleTimes;
+    public float walkRadius;
+}
