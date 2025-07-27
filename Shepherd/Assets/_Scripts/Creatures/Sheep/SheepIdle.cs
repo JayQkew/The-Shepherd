2fbda59a9345f791224c98a1f921cf4fa7bb8eb5
JayQkew <rayjebb@gmail.com>
2025-07-27 17:31:16 +0200

    adding substate

--- Shepherd/Assets/_Scripts/Creatures/Sheep/SheepIdle.cs
+++ Shepherd/Assets/_Scripts/Creatures/Sheep/SheepIdle.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 
-public class SheepIdle : SheepBaseState
+public class SheepIdle : SheepParentState
 {
     public override void EnterState(SheepStateManager manager) {
         throw new System.NotImplementedException();
     }
 
     public override void UpdateState(SheepStateManager manager) {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void HandleParentStateLogic(SheepStateManager manager) {
         throw new System.NotImplementedException();
     }
 
--- Shepherd/Assets/_Scripts/Creatures/Sheep/SheepStateManager.cs
+++ Shepherd/Assets/_Scripts/Creatures/Sheep/SheepStateManager.cs
@@ -27,3 +27,33 @@ public abstract class SheepBaseState
     public abstract void UpdateState(SheepStateManager manager);
     public abstract void ExitState(SheepStateManager manager);
 }
+
+public abstract class SheepParentState : SheepBaseState
+{
+    protected SheepBaseState currSubstate;
+
+    public virtual void SwitchSubstate(SheepBaseState newSubstate, SheepStateManager manager) {
+        currSubstate?.ExitState(manager);
+        currSubstate = newSubstate;
+        currSubstate?.EnterState(manager);
+    }
+
+    public override void EnterState(SheepStateManager manager) {
+        HandleParentEnterLogic(manager);
+        currSubstate?.EnterState(manager);
+    }
+
+    public override void ExitState(SheepStateManager manager) {
+        HandleParentExitLogic(manager);
+        currSubstate?.ExitState(manager);
+    }
+
+    public override void UpdateState(SheepStateManager manager) {
+        HandleParentStateLogic(manager);
+        currSubstate?.UpdateState(manager);
+    }
+    
+    protected abstract void HandleParentStateLogic(SheepStateManager manager);
+    protected virtual void HandleParentEnterLogic(SheepStateManager manager) {}
+    protected virtual void HandleParentExitLogic(SheepStateManager manager) {}
+}
