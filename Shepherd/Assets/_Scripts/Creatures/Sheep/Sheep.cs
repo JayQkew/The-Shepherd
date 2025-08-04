496a41fbd2619915e418c9078401aeb529722d0d
JayQkew <rayjebb@gmail.com>
2025-08-04 21:39:33 +0200

    making sheep poop

--- Shepherd/Assets/_Scripts/Creatures/Sheep/Sheep.cs
+++ Shepherd/Assets/_Scripts/Creatures/Sheep/Sheep.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class Sheep : MonoBehaviour, IBarkable
@@ -11,13 +13,21 @@ public class Sheep : MonoBehaviour, IBarkable
     [SerializeField] private float wool;
     [SerializeField] private float food;
     [SerializeField] private MinMax eat;
+
+    [Header("Poop")]
+    [SerializeField] private GameObject poopPref;
+    [SerializeField] private Transform poopSpawn;
+    [SerializeField] private float poopForce;
     [SerializeField] private bool canPoop;
+    [SerializeField] private float poopDelay;
+    [SerializeField] private float poopThreshold;
     
     [SerializeField] private float currWeight;
     [SerializeField] private float maxWeight;
 
     private void Awake() {
         rb = GetComponent<Rigidbody>();
+        canPoop = true;
     }
 
     public void BarkedAt(Vector3 sourcePosition) {
@@ -33,12 +43,28 @@ public class Sheep : MonoBehaviour, IBarkable
 
     public void Eat() {
         food += eat.RandomValue();
-        canPoop = food >= 0.85f;
+        canPoop = food >= poopThreshold;
+        
+        if (Mathf.Approximately(food, 1)) Poop();
+        else if (food >= poopThreshold && canPoop) {
+            float rand = Random.Range(0f, 1f);
+            if (rand <= food) StartCoroutine(PoopDelay());
+        }
     }
 
     private void Poop() {
         food -= Random.Range(0.3f, food);
         Debug.Log(name + " just POOOOOOPED");
+        Rigidbody poopRb = Instantiate(poopPref, poopSpawn.position, Quaternion.identity, transform).GetComponent<Rigidbody>();
+        poopRb.AddForce(poopForce * poopSpawn.up, ForceMode.Impulse);
         //play poop effects
+    }
+
+    private IEnumerator PoopDelay() {
+        canPoop = false;
+        float rand = Random.Range(0f, 1f);
+        yield return new WaitForSeconds(rand * poopDelay);
+        Poop();
+        canPoop = true;
     }
 }
