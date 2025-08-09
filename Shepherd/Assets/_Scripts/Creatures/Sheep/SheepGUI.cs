3721e86f01f8a754376cbf7f8c334f0d5948e85b
JayQkew <rayjebb@gmail.com>
2025-08-09 15:43:06 +0200

    adding sheep gui ogic

---
+++ Shepherd/Assets/_Scripts/Creatures/Sheep/SheepGUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class SheepGUI : MonoBehaviour
+{
+    [SerializeField] private GameObject[] allWool;
+
+    [SerializeField] private GameObject[] smallWool;
+    [SerializeField] private GameObject[] mediumWool;
+    [SerializeField] private GameObject[] largeWool;
+
+    public void ChangeWool(WoolLength woolLength) {
+        switch (woolLength) {
+            case WoolLength.Small:
+                ActivateWool(smallWool);
+                break;
+            case WoolLength.Medium:
+                ActivateWool(mediumWool);
+                break;
+            case WoolLength.Large:
+                ActivateWool(largeWool);
+                break;
+        }
+    }
+
+    private void ActivateWool(GameObject[] wool) {
+        foreach (GameObject w in allWool) {
+            w.SetActive(false);
+        }
+
+        foreach (GameObject w in wool) {
+            w.SetActive(true);
+        }
+    }
+
+    public enum WoolLength
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
