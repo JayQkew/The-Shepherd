8a0c06223c94ace19576bfc600eb2ab124fc55ab
JayQkew <rayjebb@gmail.com>
2025-08-09 16:24:01 +0200

    added a gui flip script

---
+++ Shepherd/Assets/_Scripts/SpriteSystem/FlipGUI.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class FlipGUI : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float flipThresh;
+
+    private Vector3 prevVel;
+
+    private void FixedUpdate() {
+        FlipCheck();
+    }
+
+    private void FlipCheck() {
+        float xVel = rb.linearVelocity.x;
+        if(prevVel.x > -flipThresh && xVel <= -flipThresh) transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        if(prevVel.x < flipThresh && xVel >= flipThresh) transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        
+        prevVel = rb.linearVelocity;
+    }
+}
