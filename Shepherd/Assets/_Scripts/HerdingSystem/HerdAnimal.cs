1ed74c974245b2ce6394aa1cfd6ce2ef765429b6
JayQkew <rayjebb@gmail.com>
2025-08-20 10:56:50 +0200

    Starting Herding system

---
+++ Shepherd/Assets/_Scripts/HerdingSystem/HerdAnimal.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class HerdAnimal : MonoBehaviour
+{
+    public HerdAreaName currHerdArea;
+}
---
+++ Shepherd/Assets/_Scripts/HerdingSystem/HerdArea.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HerdArea : MonoBehaviour
+{
+    [SerializeField] private HerdAreaName areaName;
+    public List<HerdAnimal> animalsIn = new List<HerdAnimal>();
+
+    private void OnTriggerEnter(Collider other) {
+        HerdAnimal animal = other.GetComponent<HerdAnimal>();
+        if (animal) {
+            animalsIn.Add(other.GetComponent<HerdAnimal>());
+            animal.currHerdArea = areaName;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        HerdAnimal animal = other.GetComponent<HerdAnimal>();
+        if (animal) {
+            animalsIn.Remove(animal);
+            animal.currHerdArea = HerdAreaName.None;
+        }
+    }
+}
