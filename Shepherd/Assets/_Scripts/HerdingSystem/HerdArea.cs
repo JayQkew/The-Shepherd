ec3c55dd756c31dcd594f4e9e8a7e68826aeb003
JayQkew <rayjebb@gmail.com>
2025-08-20 16:25:29 +0200

    updating herd area

--- Shepherd/Assets/_Scripts/HerdingSystem/HerdArea.cs
+++ Shepherd/Assets/_Scripts/HerdingSystem/HerdArea.cs
@@ -6,12 +6,17 @@ public class HerdArea : MonoBehaviour
 {
     public HerdAreaName areaName;
     public List<HerdAnimal> animalsIn = new List<HerdAnimal>();
+    public Dictionary<HerdAnimalName, List<HerdAnimal>> animalsByType = new Dictionary<HerdAnimalName, List<HerdAnimal>>();
 
     private void OnTriggerEnter(Collider other) {
         HerdAnimal animal = other.GetComponent<HerdAnimal>();
         if (animal) {
             animalsIn.Add(other.GetComponent<HerdAnimal>());
             animal.currHerdArea = areaName;
+            if (!animalsByType.ContainsKey(animal.animalName)) {
+                animalsByType[animal.animalName] = new List<HerdAnimal>();
+            }
+            animalsByType[animal.animalName].Add(animal);
         }
     }
 
@@ -20,6 +25,9 @@ public class HerdArea : MonoBehaviour
         if (animal) {
             animalsIn.Remove(animal);
             animal.currHerdArea = HerdAreaName.None;
+            if (animalsByType.TryGetValue(animal.animalName, out var list)) {
+                list.Remove(animal);
+            }
         }
     }
 }
