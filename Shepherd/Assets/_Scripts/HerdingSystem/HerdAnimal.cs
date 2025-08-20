c90649b21c666f82ab6a4bf5fcb309841eff375c
JayQkew <rayjebb@gmail.com>
2025-08-20 11:19:51 +0200

    adding herd manager code for managing all animals

--- Shepherd/Assets/_Scripts/HerdingSystem/HerdAnimal.cs
+++ Shepherd/Assets/_Scripts/HerdingSystem/HerdAnimal.cs
@@ -1,6 +1,16 @@
+using System;
 using UnityEngine;
 
 public class HerdAnimal : MonoBehaviour
 {
+    public HerdAnimalName animalName;
     public HerdAreaName currHerdArea;
+
+    private void OnEnable() {
+        HerdManager.Instance.AddHerdAnimal(this);
+    }
+
+    private void OnDisable() {
+        HerdManager.Instance.RemoveHerdAnimal(this);
+    }
 }
--- Shepherd/Assets/_Scripts/HerdingSystem/HerdManager.cs
+++ Shepherd/Assets/_Scripts/HerdingSystem/HerdManager.cs
@@ -1,17 +1,43 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HerdManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public static HerdManager Instance { get; private set; }
+    
+    public List<HerdAnimal> allHerdAnimals = new List<HerdAnimal>();
+    
+    public Dictionary<HerdAnimalName, List<HerdAnimal>> animalsByType = new Dictionary<HerdAnimalName, List<HerdAnimal>>();
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+        else {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void AddHerdAnimal(HerdAnimal herdAnimal) {
+        if (herdAnimal == null) return;
+    
+        allHerdAnimals.Add(herdAnimal);
+
+        if (!animalsByType.ContainsKey(herdAnimal.animalName)) {
+            animalsByType[herdAnimal.animalName] = new List<HerdAnimal>();
+        }
+        animalsByType[herdAnimal.animalName].Add(herdAnimal);
+    }
+
+    public void RemoveHerdAnimal(HerdAnimal herdAnimal) {
+        if (herdAnimal == null) return;
+
+        allHerdAnimals.Remove(herdAnimal);
+
+        if (animalsByType.TryGetValue(herdAnimal.animalName, out var list)) {
+            list.Remove(herdAnimal);
+        }
     }
 }
 
@@ -20,4 +46,10 @@ public enum HerdAreaName
     None,
     Pen,
     Field
+}
+
+public enum HerdAnimalName
+{
+    Sheep,
+    Ducken
 }
