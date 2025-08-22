f9386f33bd2c2d91d20e6067eecb4404c9901587
JayQkew <rayjebb@gmail.com>
2025-08-22 09:28:16 +0200

    adding a herd direction method

--- Shepherd/Assets/_Scripts/HerdingSystem/HerdAssist.cs
+++ Shepherd/Assets/_Scripts/HerdingSystem/HerdAssist.cs
@@ -3,17 +3,30 @@ using UnityEngine;
 
 public class HerdAssist : MonoBehaviour
 {
-    [SerializeField] private float assistMult;
+    [SerializeField] private HerdDirection direction;
+    [SerializeField] private float centerMult;
+    [SerializeField] private float pushMult;
+
+    private void PushDirection(Rigidbody rb, Vector3 dir, float forceMult) {
+        Vector3 localForce = dir.normalized;
+        Vector3 worldForce = transform.TransformDirection(localForce);
+        rb.AddForce(worldForce * forceMult, ForceMode.Impulse);
+    }
 
     private void OnTriggerStay(Collider other) {
         HerdAnimal h = other.gameObject.GetComponent<HerdAnimal>();
-        if (h != null) {
+        if (h != null && direction != HerdDirection.None) {
             Vector3 localPos = transform.InverseTransformPoint(h.transform.position);
-            float xDir = -localPos.x;
-            Vector3 localForce = new Vector3(xDir, 0, 0).normalized;
-            Vector3 worldForce = transform.TransformDirection(localForce);
-            
-            h.rb.AddForce(worldForce * assistMult, ForceMode.Impulse);
+            PushDirection(h.rb, new Vector3(-localPos.x, 0, 0), centerMult);
+            if(direction == HerdDirection.In) PushDirection(h.rb, -transform.forward, pushMult);
+            else PushDirection(h.rb, transform.forward, pushMult);
         }
+    }
+
+    public enum HerdDirection
+    {
+        None,
+        In,
+        Out
     }
 }
