1df9f158fef250e3573a133e9f85bbc0ca2bea72
JayQkew <rayjebb@gmail.com>
2025-08-25 20:39:22 +0200

    fence opening and closing animated

--- Shepherd/Assets/_Scripts/HerdingSystem/HerdGate.cs
+++ Shepherd/Assets/_Scripts/HerdingSystem/HerdGate.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using UnityEngine.Splines;
+using System;
 
 public class HerdGate : MonoBehaviour
 {
     [SerializeField] private SplineContainer splineContainer;
     [SerializeField] private BezierKnot meetingKnot;
     [SerializeField] private BezierKnot[] openKnots;
+    [SerializeField] private float animationDuration = 1f;
+
+    private Timer gateTimer;
+    private BezierKnot start0, start1;   // starting knots for animation
+    private BezierKnot target0, target1; // target knots for animation
+    private bool isAnimating;
 
     private void Awake() {
         splineContainer = GetComponent<SplineContainer>();
@@ -18,9 +25,23 @@ public class HerdGate : MonoBehaviour
         CalculateMeetingKnot();
     }
 
-    private void OnValidate() {
-        if (splineContainer != null && splineContainer.Spline.Count > 1) {
-            CalculateMeetingKnot();
+    private void Update() {
+        if (isAnimating) {
+            gateTimer.Update();
+
+            float t = gateTimer.Progress;
+            BezierKnot lerped0 = LerpKnot(start0, target0, t);
+            BezierKnot lerped1 = LerpKnot(start1, target1, t);
+
+            splineContainer.Spline[0] = lerped0;
+            splineContainer.Spline[^1] = lerped1;
+
+            if (gateTimer.IsFinished) {
+                // snap to target when done
+                splineContainer.Spline[0] = target0;
+                splineContainer.Spline[^1] = target1;
+                isAnimating = false;
+            }
         }
     }
 
@@ -28,18 +49,40 @@ public class HerdGate : MonoBehaviour
         meetingKnot.Position = (splineContainer.Spline[0].Position + splineContainer.Spline[^1].Position) / 2f;
         meetingKnot.TangentIn = (splineContainer.Spline[0].TangentIn + splineContainer.Spline[^1].TangentIn) / 2f;
         meetingKnot.TangentOut = (splineContainer.Spline[0].TangentOut + splineContainer.Spline[^1].TangentOut) / 2f;
-        meetingKnot.Rotation = Quaternion.Slerp(splineContainer.Spline[0].Rotation, splineContainer.Spline[^1].Rotation, 0.5f);
+        meetingKnot.Rotation = Quaternion.Slerp(
+            splineContainer.Spline[0].Rotation,
+            splineContainer.Spline[^1].Rotation,
+            0.5f
+        );
     }
 
     [ContextMenu("OpenGate")]
     private void OpenGate() {
-        splineContainer.Spline[0] = openKnots[0];
-        splineContainer.Spline[^1] = openKnots[1];
+        StartGateAnimation(openKnots[0], openKnots[1]);
     }
 
     [ContextMenu("CloseGate")]
     private void CloseGate() {
-        splineContainer.Spline[0] = meetingKnot;
-        splineContainer.Spline[^1] = meetingKnot;
+        StartGateAnimation(meetingKnot, meetingKnot);
+    }
+
+    private void StartGateAnimation(BezierKnot newTarget0, BezierKnot newTarget1) {
+        start0 = splineContainer.Spline[0];
+        start1 = splineContainer.Spline[^1];
+        target0 = newTarget0;
+        target1 = newTarget1;
+
+        gateTimer.SetMaxTime(animationDuration, resetTimer: true);
+        isAnimating = true;
+    }
+
+    private BezierKnot LerpKnot(BezierKnot a, BezierKnot b, float t) {
+        BezierKnot result = new BezierKnot {
+            Position = Vector3.Lerp(a.Position, b.Position, t),
+            TangentIn = Vector3.Lerp(a.TangentIn, b.TangentIn, t),
+            TangentOut = Vector3.Lerp(a.TangentOut, b.TangentOut, t),
+            Rotation = Quaternion.Slerp(a.Rotation, b.Rotation, t)
+        };
+        return result;
     }
 }
