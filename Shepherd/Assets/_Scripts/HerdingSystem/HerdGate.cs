0151fb15bd9118c5c99ea5e40b57217ceeb3bd64
JayQkew <rayjebb@gmail.com>
2025-08-25 20:30:11 +0200

    adding opening and closing to the fence

--- Shepherd/Assets/_Scripts/HerdingSystem/HerdGate.cs
+++ Shepherd/Assets/_Scripts/HerdingSystem/HerdGate.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -13,8 +12,23 @@ public class HerdGate : MonoBehaviour
     }
 
     private void Start() {
+        openKnots = new BezierKnot[2];
         openKnots[0] = splineContainer.Spline[0];
         openKnots[1] = splineContainer.Spline[^1];
+        CalculateMeetingKnot();
+    }
+
+    private void OnValidate() {
+        if (splineContainer != null && splineContainer.Spline.Count > 1) {
+            CalculateMeetingKnot();
+        }
+    }
+
+    private void CalculateMeetingKnot() {
+        meetingKnot.Position = (splineContainer.Spline[0].Position + splineContainer.Spline[^1].Position) / 2f;
+        meetingKnot.TangentIn = (splineContainer.Spline[0].TangentIn + splineContainer.Spline[^1].TangentIn) / 2f;
+        meetingKnot.TangentOut = (splineContainer.Spline[0].TangentOut + splineContainer.Spline[^1].TangentOut) / 2f;
+        meetingKnot.Rotation = Quaternion.Slerp(splineContainer.Spline[0].Rotation, splineContainer.Spline[^1].Rotation, 0.5f);
     }
 
     [ContextMenu("OpenGate")]
