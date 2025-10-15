b02609463eecd76819dd2e15f146b5341a84e6dd
JayQkew <rayjebb@gmail.com>
2025-10-15 09:01:12 +0200

    adjusting floating rock logic

--- Shepherd/Assets/_Scripts/Environment/FloatingRock.cs
+++ Shepherd/Assets/_Scripts/Environment/FloatingRock.cs
@@ -5,7 +5,7 @@ using UnityEngine.Serialization;
 namespace Environment
 {
     [RequireComponent(typeof(Rigidbody))]
-    public class FloatingRock : MonoBehaviour
+    public class FloatingRock : MonoBehaviour, IBarkable
     {
         private Rigidbody rb;
         private Vector3 targetPos;
@@ -32,10 +32,18 @@ namespace Environment
             float zDistance = transform.position.z - targetPos.z;
             
             returnDir = new Vector3(xDistance, yDistance, zDistance) * -1;
+            
+            rb.angularVelocity = Vector3.ClampMagnitude(rb.angularVelocity, 1);
         }
 
         private void FixedUpdate() {
             rb.AddForce(returnDir.normalized * returnForce, ForceMode.Acceleration);
+            rb.AddTorque(transform.forward * returnForce, ForceMode.Force);
+        }
+
+        public void BarkedAt(Vector3 sourcePosition) {
+            Vector3 dir = (transform.position - sourcePosition).normalized;
+            rb.AddForce(dir * returnForce, ForceMode.Impulse);
         }
     }
 }
