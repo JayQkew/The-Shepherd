f9c1896e52a8a99a5615113685b8bde9264e91a3
JayQkew <rayjebb@gmail.com>
2025-11-04 11:00:24 +0200

    ading adjsutable frame rate

--- Shepherd/Assets/_Scripts/FrameManager.cs
+++ Shepherd/Assets/_Scripts/FrameManager.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 
 public class FrameManager : MonoBehaviour
 {
+    public int targetFrameRate = 60;
     private void Awake() {
 #if UNITY_EDITOR
         QualitySettings.vSyncCount = 0;     // Disable VSync so FPS cap works
-        Application.targetFrameRate = 60;   // Cap Editor FPS to 60
+        Application.targetFrameRate = targetFrameRate;   // Cap Editor FPS to 60
 #else
         QualitySettings.vSyncCount = 1;     // Use VSync in builds
 #endif
